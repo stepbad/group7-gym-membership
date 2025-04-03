a7d26667be8e2272d5a55b9a98eccf6f7b579436
Stephen Badcock <badcockstephen73@gmail.com>
2025-04-03 14:01:15 -0230

    javadoc added to trainer, no code adjustments needed

--- src/main/java/com/group7/gym/models/Trainer.java
+++ src/main/java/com/group7/gym/models/Trainer.java
@@ -4,38 +4,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Trainer subclass of User – represents a gym trainer.
+ * Represents a Trainer user in the gym system.
+ * A Trainer can be assigned to multiple workout classes.
  */
 public class Trainer extends User {
 
-    //  List of IDs for workout classes this trainer is assigned to
+    /** List of workout class IDs assigned to this trainer. */
     private List<Integer> assignedClassIds;
 
+    /**
+     * Constructs a new Trainer with the given details.
+     *
+     * @param userId        Unique user ID
+     * @param username      Username
+     * @param passwordHash  Hashed password
+     * @param email         Email address
+     * @param phone         Phone number
+     * @param address       Physical address
+     */
     public Trainer(int userId, String username, String passwordHash, String email, String phone, String address) {
         super(userId, username, passwordHash, email, phone, address, "Trainer");
         this.assignedClassIds = new ArrayList<>();
     }
 
-    // --- Methods ---
-
+    /**
+     * Assigns a workout class to the trainer by ID.
+     *
+     * @param classId ID of the class to assign
+     */
     public void assignClass(int classId) {
         if (!assignedClassIds.contains(classId)) {
             assignedClassIds.add(classId);
         }
     }
 
+    /**
+     * Removes a workout class assignment by ID.
+     *
+     * @param classId ID of the class to remove
+     */
     public void removeClass(int classId) {
         assignedClassIds.remove(Integer.valueOf(classId));
     }
 
+    /**
+     * Gets a list of assigned class IDs.
+     *
+     * @return List of workout class IDs
+     */
     public List<Integer> getAssignedClassIds() {
         return assignedClassIds;
     }
 
+    /**
+     * Sets the assigned class IDs for this trainer.
+     *
+     * @param assignedClassIds List of class IDs
+     */
     public void setAssignedClassIds(List<Integer> assignedClassIds) {
         this.assignedClassIds = assignedClassIds;
     }
 
+    /**
+     * Returns a string representation of the Trainer object.
+     *
+     * @return String summary of trainer
+     */
     @Override
     public String toString() {
         return "Trainer{" +
